501c2f2629b3d9012f2357b2b8bda15c7dc5325c
yugankkupadhyaya <yugankkupadhyaya@gmail.com>
2026-08-20 19:49:16 +0530

    aded firebase token and user creating oin backend auth

--- backend/services/auth/model/user.model.js
+++ backend/services/auth/model/user.model.js
@@ -1,36 +1,31 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema =
-new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
+    firebaseUid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-   firebaseUid:{
-      type:String,
-      required:true,
-      unique:true
-   },
+    name: { type: String, required: true },
 
-   name:String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-   email:{
-      type:String,
-      required:true,
-      unique:true
-   },
-
-
-   interviewCoin:{
-      type:Number,
-      default:150
-   }
-
-},{
-   timestamps:true
-});
-
- const User =
-mongoose.model(
-   "User",
-   userSchema
+    interviewCoin: {
+      type: Number,
+      default: 150,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
+
+const User = mongoose.model('User', userSchema);
 
 export default User;
